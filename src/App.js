c4ca1eec9862a76616f7af2f456cc0409a4c1be6
Tim Cartwright <tim@teasea.uk>
2020-02-09 19:38:33 +0000

    Text editor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import { Editor, createEditorState, BLOCK_BUTTONS } from 'medium-draft'
+import '../node_modules/medium-draft/lib/index.css';
+import mediumDraftExporter from 'medium-draft/lib/exporter';
 
 function App() {
+  const [editorState, setEditorState] = useState(createEditorState());
+  const renderedHTML = mediumDraftExporter(editorState.getCurrentContent());
+
+  const blockButtons = [{
+      label: 'H1',
+      style: 'header-one',
+      icon: 'header',
+      description: 'Heading 1',
+    },
+    {
+      label: 'H2',
+      style: 'header-two',
+      icon: 'header',
+      description: 'Heading 2',
+  }].concat(BLOCK_BUTTONS);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{margin: '50px'}}>
+      <Editor
+        blockButtons={blockButtons}
+        editorState={editorState}
+        onChange={setEditorState}/>
+
+        <div
+          dangerouslySetInnerHTML={{__html: renderedHTML}}
+          style ={{
+            textAlign: 'left',
+            width: '650px',
+            margin: '0 auto',
+          }}
+        />
     </div>
   );
 }
